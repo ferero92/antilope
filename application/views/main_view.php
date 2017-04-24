93f6bd8dc61d8aa8411b552becf9d975351bc5c5
ferero92 <ferero92@hotmail.com>
2017-04-24 12:24:32 +0200

    24/04/17

---
+++ application/views/main_view.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles/style.css">
+  <script src="jquery/jquery-3.2.1.min.js" charset="utf-8"></script>
+  <script src="scripts/code.js" charset="utf-8"></script>
+</head>
+<body>
+  
+</body>
+</html>
